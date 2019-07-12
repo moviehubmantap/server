5edd4e538c47ed6723556ef5299ca8ac1d25bb67
jaysm12 <jaizm12@gmail.com>
2019-07-12 13:48:20 +0700

    mantap

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const app = express();
-const routes = require('./routes');
 const port = 3000;
 const cors = require('cors');
-const morgan = require('morgan')
+const axios = require('axios')
+const tasteDiveAPI = 'https://tastedive.com/api/similar?type=movies&k=340179-movieHub-6N3RBPCP&limit=10&info=1&q='
 require('dotenv').config()
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-app.use(morgan())
 app.use(cors());
-app.use('/api', routes)
 
 const mongoose = require('mongoose');
-const url = 'mongodb://localhost:27017/project'
+const url = 'mongodb://localhost:27017/moviehub'
 mongoose.connect(url, {useNewUrlParser: true}, (err) => {
   if(err) {
     console.log(err)
@@ -23,18 +21,20 @@ mongoose.connect(url, {useNewUrlParser: true}, (err) => {
   }
 })
 
-app.use(function(err,req,res,next){
-  console.log(err)
-  if(err.code === 404) {
-    res.status(404).json({ message: 'Resource not found' })
-  } else if(err.name === 'ValidationError') {
-    res.status(500).json({ message: err.message })
-  } else {
-    const status = err.status || 500
-    const message = err.message
-    res.status(status).json({ message: message })
-  }
-});
+app.get('/similiar/:title', (req, res) => {
+  axios({
+    method: 'GET',
+    url: `${tasteDiveAPI}${req.params.title}`
+  })
+  .then(({data}) => {
+    res.status(200).json(data)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+})
+
+
 
 app.listen(port, () => console.log(`listening on port port`))
 
